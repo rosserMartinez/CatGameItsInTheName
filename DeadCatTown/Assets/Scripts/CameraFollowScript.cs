8646fe23e3d2150fbf27d6f16d484a1dfdda0cde
rosserMartinez <rosser.martinez@gmail.com>
2017-10-07 01:05:19 -0400

    smooth camera follow

---
+++ DeadCatTown/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+	private GameObject catToFollow;
+	private Transform catCameraTarget;
+	public float bigSmooth;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+			
+		catToFollow = GameObject.Find("cameraRange");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 moveCamera = new Vector3(catToFollow.transform.position.x, catToFollow.transform.position.y, catToFollow.transform.position.z);
+
+		transform.position = Vector3.Lerp(transform.position, moveCamera, bigSmooth * Time.deltaTime);
+
+
+		///////////////////////to track astral, lerp to midpoint instead of cat itself
+	}
+}
