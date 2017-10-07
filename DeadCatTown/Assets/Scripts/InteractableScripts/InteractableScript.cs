6f72c57583b7e3d850fd3a84710fb5f2f25d3ea1
linuxlab <cory.smith@mymail.champlain.edu>
2017-10-07 16:31:05 -0400

    Highlighting objects you can interact with

--- DeadCatTown/Assets/Scripts/InteractableScripts/InteractableScript.cs
+++ DeadCatTown/Assets/Scripts/InteractableScripts/InteractableScript.cs
@@ -2,33 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class InteractableScript : MonoBehaviour {
 
 	public bool IsInteractableByPhysicalCat = false;
+    MeshRenderer meshRenderer;
+    float defaultMetallic;
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        defaultMetallic = meshRenderer.material.GetFloat("_Metallic");
+    }
 
-	void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider col)
 	{
-		if (IsInteractableByPhysicalCat = true && col.gameObject.tag == "PhysicalCat") 
+		if (IsInteractableByPhysicalCat == true && col.gameObject.tag == "PhysicalCat" ||
+            IsInteractableByPhysicalCat == false && col.gameObject.tag == "AstralCat" ) 
 		{
 			SendMessage ("EnteredObjectRange", col.gameObject);
-		}
 
-		if (IsInteractableByPhysicalCat = true && col.gameObject.tag == "AstralCat") 
-		{
-			SendMessage ("EnteredObjectRange", col.gameObject);
-		}
-	}
+            meshRenderer.material.SetFloat("_Metallic", 1);
+
+        }
+    }
 
 	void OnTriggerExit(Collider col)
 	{
-		if (IsInteractableByPhysicalCat = true && col.gameObject.tag == "PhysicalCat") 
+		if (IsInteractableByPhysicalCat == true && col.gameObject.tag == "PhysicalCat" ||
+            IsInteractableByPhysicalCat == false && col.gameObject.tag == "AstralCat") 
 		{
 			SendMessage ("ExitedObjectRange", col.gameObject);
-		}
 
-		if (IsInteractableByPhysicalCat = true && col.gameObject.tag == "AstralCat") 
-		{
-			SendMessage ("ExitedObjectRange", col.gameObject);
-		}
-	}
+            meshRenderer.material.SetFloat("_Metallic", defaultMetallic);
+        }
+    }
 }
