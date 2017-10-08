4264704c87ff8a2d4457a8f825c0099453243b6a
rosserMartinez <rosser.martinez@gmail.com>
2017-10-08 14:18:22 -0400

    commiting missing menu stuff

---
+++ DeadCatTown/Assets/Scripts/MainMenuScripts/ManageMenuUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ManageMenuUI : MonoBehaviour {
+
+	public Canvas MainMenuCanvas;
+	public Canvas TutorialCanvas;
+	public Canvas CreditsCanvas;
+
+
+	// Use this for initialization
+	void Start () {
+		TutorialCanvas.enabled = false;
+		CreditsCanvas.enabled = false;
+		MainMenuCanvas.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene ("TutorialScene");
+	}
+
+	public void ToTutorialCanvas()
+	{
+		MainMenuCanvas.enabled = false;
+		CreditsCanvas.enabled = false;
+		TutorialCanvas.enabled = true; 
+	}
+	public void ToMainMenuCanvas()
+	{
+		MainMenuCanvas.enabled = true;
+		CreditsCanvas.enabled = false;
+		TutorialCanvas.enabled = false; 
+	}
+	public void ToCreditsCanvas()
+	{
+		MainMenuCanvas.enabled = false;
+		CreditsCanvas.enabled = true;
+		TutorialCanvas.enabled = false; 
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
