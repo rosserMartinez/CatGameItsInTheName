6aa3498e7c2920453d51610ea2593f7e86a920d3
linuxlab <cory.smith@mymail.champlain.edu>
2017-10-07 23:00:29 -0400

    First pass on audio system

---
+++ DeadCatTown/Assets/Scripts/DestroyOnAudioStop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DestroyOnAudioStop : MonoBehaviour
+{
+    AudioSource source;
+
+	// Use this for initialization
+	void Start ()
+    {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!source.isPlaying)
+            Destroy(gameObject);
+	}
+}
